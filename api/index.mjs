0e32cd7180289222f261949ca5cb6aed94c62761
SharkyX99 <yonuttanawat6933@gmail.com>
2025-12-23 10:53:16 +0700

    Update backend functionality for improved performance

--- api/index.mjs
+++
@@ -1,6 +0,0 @@
-import serverless from "serverless-http";
-import { createRequire } from "module";
-const require = createRequire(import.meta.url);
-const { app } = require("../index.js");
-
-export default serverless(app);
