733160eef2beca61724fec826b41391d3a604814
VacTuzX-dot <lilitxrie@gmail.com>
2026-01-19 18:53:05 +0700

    Fix: Correct swagger import path and update vercel config

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 const express = require("express");
 const cors = require("cors");
 const swaggerUi = require("swagger-ui-express");
-const swaggerSpec = require("./docs/swagger");
-const authRoutes = require('./routes/auth');
+const swaggerSpec = require("./swagger");
+const authRoutes = require("./routes/auth");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use('/api/auth', authRoutes);
+app.use("/api/auth", authRoutes);
 
 app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 app.get("/docs/swagger.json", (req, res) => {
-    res.json(swaggerSpec);
+  res.json(swaggerSpec);
 });
 
 app.use("/login", require("./routes/auth.routes"));
